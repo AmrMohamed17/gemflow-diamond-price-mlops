e74c9c2e673060b33cab7ea8a7c297855590e9c3
Amr Mohammed <abdelrahman.mohamed89@msa.edu.eg>
2025-11-18 09:47:18 +0200

    latest updates

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -1,23 +1,26 @@
 import pandas as pd
+import os
 from dataclasses import dataclass
 from sklearn.model_selection import train_test_split
 
 @dataclass
 class DataIngestionConfig:
-  data_path: str = "artifacts/data.csv"
-
+    data_path: str = "artifacts/data.csv"
 
 class DataIngestion:
-  def __init__(self) -> None:
-    self.data_config = DataIngestionConfig()
+    def __init__(self) -> None:
+        self.data_config = DataIngestionConfig()
 
-  def initiate_data_ingestion(self):
-    try:
-      print("Data Ingestion Started")
-      data = pd.read_csv(self.data_config.data_path)
-      data = data.drop(columns=['id'])
-      train, test = train_test_split(data, test_size=0.25)
-      return train, test
-
-    except Exception as e:
-      print(e)
+    def initiate_data_ingestion(self):
+        try:
+            print("Data Ingestion Started")
+            data = pd.read_csv(self.data_config.data_path)
+            data = data.drop(columns=['id'])
+            train, test = train_test_split(data, test_size=0.25, random_state=42)
+            
+            # Save intermediate files for DVC
+            os.makedirs("artifacts", exist_ok=True)
+            
+            return train, test
+        except Exception as e:
+            print(e)
--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -1,13 +1,17 @@
+import json
 import os
 import pandas as pd
 from dataclasses import dataclass
+
+from sklearn.pipeline import _name_estimators
 from src.utils.utils import evaluate_models
 from sklearn.tree import DecisionTreeRegressor
 from sklearn.linear_model import LinearRegression
 from xgboost import XGBRegressor
 from src.utils.utils import save_object
 import mlflow
-import dagshub
+import yaml
+
 
 @dataclass
 class ModelTrainerConfig:
@@ -21,10 +25,16 @@ class ModelTrainer:
   def initiate_model_trainer(self, X_train, X_test, y_train, y_test, test=False):
     print("Model Trainer Started")
     try:
+      with open('params.yaml', 'r') as f:
+          params = yaml.safe_load(f)
+
+      model_params = params['model']    
       models = {
         # 'LinearRegression': LinearRegression(),
         # 'DecisionTreeRegressor': DecisionTreeRegressor(),
-        'XGBRegressor': XGBRegressor(n_estimators=300, learning_rate=0.05, max_depth=4)
+        'XGBRegressor': XGBRegressor(n_estimators= model_params['n_estimators'], 
+                                    learning_rate= model_params['learning_rate'], 
+                                    max_depth = model_params['max_depth'])
       }
 
       model_report = evaluate_models(X_train, X_test, y_train, y_test,models)
@@ -36,22 +46,31 @@ class ModelTrainer:
       print(f'Best Model Found , Model Name : {best_model_name} , R2 Score : {best_model_score}')
 
       if not test:
-        dagshub.init(repo_owner="AmrMohamed17", repo_name='gemflow-diamond-price-mlops', mlflow=True)
-        mlflow.set_tracking_uri("https://dagshub.com/AmrMohamed17/gemflow-diamond-price-mlops.mlflow")
-        mlflow.set_experiment("DiamondPricePred")
-        with mlflow.start_run():
-          mlflow.log_params({
-            "n_estimators": 300,
-            "learning_rate": 0.05, 
-            "max_depth" : 4
-          })
+        # dagshub.init(repo_owner="AmrMohamed17", repo_name='gemflow-diamond-price-mlops', mlflow=True)
+        # mlflow.set_tracking_uri("https://dagshub.com/AmrMohamed17/gemflow-diamond-price-mlops.mlflow")
+        # mlflow.set_experiment("DiamondPricePred")
+        # with mlflow.start_run():
+        #   mlflow.log_params({
+        #     "n_estimators": 300,
+        #     "learning_rate": 0.05, 
+        #     "max_depth" : 4
+        #   })
 
-          # best_model = models[best_model_name]
-          # best_model.fit(X_train, y_train)
+        #   # best_model = models[best_model_name]
+        #   # best_model.fit(X_train, y_train)
 
-          mlflow.log_metric("r2_score", best_model_score)
+        #   mlflow.log_metric("r2_score", best_model_score)
 
-          # mlflow.xgboost.log_model(models["XGBRegressor"], "model")
+        #   # mlflow.xgboost.log_model(models["XGBRegressor"], "model")
+
+        metrics = {
+            "r2_score": float(best_model_score),
+            "best_model": best_model_name
+        }
+
+        os.makedirs('artifacts', exist_ok=True)
+        with open('artifacts/metrics.json', 'w') as f:
+            json.dump(metrics, f, indent=4)
 
         save_object(
           file_path=self.config.model_path,
